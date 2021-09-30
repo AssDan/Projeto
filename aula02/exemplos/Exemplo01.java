6640f1444b65b299d96d57e3ee230842229046b3
Assis Daniel <assisdaniel.sales@gmail.com>
2021-09-29 22:12:39 -0300

    aula02

---
+++ aula02/exemplos/Exemplo01.java
@@ -0,0 +1,8 @@
+package exemplos;
+
+ public class Exemplo01 {
+
+    public static void main(String[] args) {
+        System.out.println("Meu");
+    }
+}
---
+++ aula02/exemplos/Exemplo02.java
@@ -0,0 +1,11 @@
+package exemplos;
+
+public class Exemplo02 {
+    public static void main(String[] args) {
+        System.out.println("Oi");
+        System.out.println(" 2 + 3 ");
+        System.out.println(2 + 3);
+        
+    }
+    
+}
