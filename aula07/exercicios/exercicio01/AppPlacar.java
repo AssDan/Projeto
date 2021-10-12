0ea66762d4954a72ef9cf972cb5bd2336f9ddb3c
AssDan <89284120+AssDan@users.noreply.github.com>
2021-10-11 21:54:44 -0300

    aula07

--- aula07/exercicios/exercicio01/AppPlacar.java
+++
@@ -1,9 +0,0 @@
-package exercicios.exercicio01;
-
-public class AppPlacar {
-    public static void main(String[] args) {
-        Placar  p1 = new Placar();
-        Placar  p1 = new Placar(Sao Paulo , Corinthians);
-
-    }
-}
